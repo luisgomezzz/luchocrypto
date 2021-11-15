e1abdbf9063e8260c8c97e912f45e4c0bb36fa7a
lucho <luchogomezf@gmail.com>
2021-11-15 17:00:37 -0300

    Mejoras, ahora se agregan las resistencias y soportes.

--- gocrypto_alarm_bot/gocrypto_alarm_bot.py
+++ gocrypto_alarm_bot/gocrypto_alarm_bot.py
@@ -5,6 +5,12 @@ import sys
 from binance.exceptions import BinanceAPIException
 import requests
 import math
+import yfinance
+from mplfinance.original_flavor import candlestick_ohlc
+import matplotlib.dates as mpl_dates
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 #!/usr/bin/env python3
 import requests
@@ -13,6 +19,67 @@ import json
 
 import talib
 import numpy as np
+
+def supportresistance(par):
+    plt.rcParams['figure.figsize'] = [12, 7]
+    plt.rc('font', size=14)
+    now = datetime.now() # current date and time
+    hoy = now.strftime("%Y-%m-%d")
+    haceNdias=(now-timedelta(days=300)).strftime("%Y-%m-%d")
+
+    output=str('-1')
+    name = par.replace("USDT", "-USD")
+    print("Searching pair in yfinance as", name) 
+
+
+    try:
+        ticker = yfinance.Ticker(name)
+        df = ticker.history(interval="1d",start=haceNdias, end=hoy)
+        df['Date'] = pd.to_datetime(df.index)
+        df['Date'] = df['Date'].apply(mpl_dates.date2num)
+        df = df.loc[:,['Date', 'Open', 'High', 'Low', 'Close']]
+
+        def isSupport(df,i):
+            support = df['Low'][i] < df['Low'][i-1]  and df['Low'][i] < df['Low'][i+1] and df['Low'][i+1] < df['Low'][i+2] and df['Low'][i-1] < df['Low'][i-2]
+            return support
+        def isResistance(df,i):
+            resistance = df['High'][i] > df['High'][i-1]  and df['High'][i] > df['High'][i+1] and df['High'][i+1] > df['High'][i+2] and df['High'][i-1] > df['High'][i-2]
+            return resistance
+
+        def plot_all():
+            fig, ax = plt.subplots()
+            candlestick_ohlc(ax,df.values,width=0.6,colorup='green', colordown='red', alpha=0.8)
+            date_format = mpl_dates.DateFormatter('%d %b %Y')
+            ax.xaxis.set_major_formatter(date_format)
+            fig.autofmt_xdate()
+            fig.tight_layout()
+            for level in levels:
+                plt.hlines(level[1],xmin=df['Date'][level[0]], xmax=max(df['Date']),colors='blue')
+            fig.show()    
+
+        s =  np.mean(df['High'] - df['Low'])
+
+        def isFarFromLevel(l):
+            return np.sum([abs(l-x) < s  for x in levels]) == 0
+
+        levels = []
+        for i in range(2,df.shape[0]-2):
+            if isSupport(df,i):
+                l = df['Low'][i]
+                if isFarFromLevel(l):
+                    levels.append((i,l))
+            elif isResistance(df,i):
+                l = df['High'][i]
+                if isFarFromLevel(l):
+                    levels.append((i,l))
+            
+        output= str([x[1] for x in levels])
+    except:
+        output= str('-1')
+        pass
+
+    return output
+
 
 def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
@@ -106,20 +173,24 @@ def main() -> None:
 
                         if ((precioactual - preciomenor)*(100/preciomenor))>=porcentaje and (precioactual>=preciomayor) and float(volumen24h)>=float(1):
                             #os.system('play -nq -t alsa synth %s sin %s' % (duration/1000, freq))
-                            #input("Press Enter to continue...")     
+                            #input("Press Enter to continue...")
+                            print("paso1")     
                             mensaje=par+" up "+str(round(((precioactual - preciomenor)*(100/preciomenor)),2))+"% - "+str(ventana)+" minutes. RSI14: "+str(truncate(rsi14(client,par),2))
-                            print (mensaje)
                             mandomensaje (mensaje,idgrupo)
-                            mandomensaje (mensaje,chatid)                             
+                            mandomensaje ("Supports and resistances:"+supportresistance(par),idgrupo)
+                            mandomensaje (mensaje,chatid)
+                            mandomensaje ("Supports and resistances:"+supportresistance(par),chatid)
 
                         if ((preciomenor - precioactual)*(100/preciomenor))>=porcentaje and (precioactual<=preciomenor) and float(volumen24h)>=float(1):
                             #os.system('play -nq -t alsa synth %s sin %s' % (duration/1000, freq))
                             #input("Press Enter to continue...")     
+                            print("paso2")
                             mensaje=par+" down "+str(round(((preciomenor - precioactual)*(100/preciomenor)),2))+"% - "+str(ventana)+" minutes. RSI14: "+str(truncate(rsi14(client,par),2))
-                            print (mensaje)
                             mandomensaje (mensaje,idgrupo)
-                            mandomensaje (mensaje,chatid) 
-                        
+                            mandomensaje ("Supports and resistances:"+supportresistance(par),idgrupo)
+                            mandomensaje (mensaje,chatid)
+                            mandomensaje ("Supports and resistances:"+supportresistance(par),chatid)
+
                         sys.stdout.write("\rBuscando oportunidad. Ctrl+c para salir. Par: "+par+"\033[K")
                         sys.stdout.flush()
                         
