9f32188a993795e98103653a0a991c415af14157
lucho <luchogomezf@gmail.com>
2021-11-21 09:39:50 -0300

    Corrección en periodos.

--- taller/taller.py
+++ taller/taller.py
@@ -18,11 +18,13 @@ binance_api="N7yU75L3CNJg2RW0TcJBAW2cUjhPGvyuSFUgnRHvMSMMiS8WpZ8Yd8yn70evqKl0"
 binance_secret="2HfMkleskGwTb6KQn0AKUQfjBDd5dArBW3Ykd2uTeOiv9VZ6qSU2L1yWM1ZlQ5RH"
 par ='LRCUSDT'
 tr.clear()
-historicdf=tr.historicdf(par)
 chatid="@gofrecrypto" #canal
 idgrupo = "-704084758" #grupo de amigos
 token = "2108740619:AAHcUBakZLdoHYnvUvkBp6oq7SoS63erb2g"
 botlaburo = TelegramBot(token, chatid)
 
+
+suddendf=tr.historicdf(par,timeframe='1m',limit=60)
 ###########################################################
 
+print(tr.truncate(suddendf.ta.rsi().iloc[-1],2))
