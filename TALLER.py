4afcb9e91680fa4e7d0e800a0337dde70914cf9b
lucho <luchogomezf@gmail.com>
2022-04-15 13:10:33 -0300

    pruebas

--- TALLER.py
+++ TALLER.py
@@ -26,13 +26,23 @@ posicion=[0,'NADA']
 exchange=ut.binanceexchange(ut.binance_api,ut.binance_secret) #login
 
 
+#df2=pd.DataFrame(df['time']/1000)
+#df2=df2.set_axis(['time2'], axis=1, inplace=False)
+
+
+def timeindex(df):
+   # if you encounter a "year is out of range" error the timestamp
+   # may be in milliseconds, try `ts /= 1000` in that case
+   df2=pd.DataFrame(df['time']/1000) #df['time2']=df['time']/1000
+   df3 = df2.rename(columns = {'time': 'time2'}, inplace = False)
+   df4=pd.concat([df, df3])
+   #df4 = df3.rename(columns = {'time': 'time2'}, inplace = False)
+   #df5=pd.DataFrame(pd.to_datetime(df3['time2'],unit='s')) 
+   print(df4)
+   
+
+   
+
 df=ut.binancehistoricdf(par,timeframe=temporalidad,limit=ventana) # para fractales.
-
-###CODIGO
-ut.timeindex(df) #Formatea el campo time para luego calcular las señales
-df.ta.study() # Runs and appends all indicators to the current DataFrame by default
-
-ut.komucloud (df)
-
-
-print(df.ta.cci(40))
+timeindex(df) #Formatea el campo time para luego calcular las señales
+#df.ta.strategy() # Runs and appends all indicators to the current DataFrame by default
