03989742cc8f4cf457a019021bdf0744f265d06f
lucho <luchogomezf@gmail.com>
2022-05-08 21:12:53 -0300

    mejoras

--- psar.py
+++ psar.py
@@ -66,50 +66,42 @@ def main() -> None:
                         sys.stdout.flush()
                         
                         df=ut.calculardf (par,temporalidad,ventana)
-
                         crosshigh=(pdta.xsignals(df.ta.cci(40),100,100,above=True)).iloc[-1]
                         crosslow=(pdta.xsignals(df.ta.cci(40),-100,-100,above=True)).iloc[-1]
+
                         #CRUCE ARRIBA
-                        if  (((crosshigh[0]==1 and crosshigh[1]==1 and crosshigh[2]==1 and crosshigh[3]==0) 
-                            or (crosslow[0]==1 and crosslow[1]==1 and crosslow[2]==1 and crosslow[3]==0))
-                            and 50>df.ta.stochrsi()['STOCHRSIk_14_14_3_3'].iloc[-1]>df.ta.stochrsi()['STOCHRSId_14_14_3_3'].iloc[-1]
-                            ):
-
-                            ut.komucloud (df)
-                            
-                            currentprice = float(client.get_symbol_ticker(symbol=par)["price"])
-                            if (df['signal'].iloc[-1]==1 
-                                and (df['signal'].iloc[-2]==0 or df['signal'].iloc[-2]==-1)
-                                and currentprice > df.ta.ema(50).iloc[-1] > df.ta.ema(200).iloc[-1] 
+                        if float(client.get_symbol_ticker(symbol=par)["price"]) > df.ta.ema(50).iloc[-1] > df.ta.ema(200).iloc[-1]:
+                            if  (((crosshigh[0]==1 and crosshigh[1]==1 and crosshigh[2]==1 and crosshigh[3]==0) 
+                                or (crosslow[0]==1 and crosslow[1]==1 and crosslow[2]==1 and crosslow[3]==0))
+                                and 50>df.ta.stochrsi()['STOCHRSIk_14_14_3_3'].iloc[-1]>df.ta.stochrsi()['STOCHRSId_14_14_3_3'].iloc[-1]
                                 ):
 
-                                posicioncreada=ut.posicionfuerte(par,'BUY',client)                                
-                                if posicioncreada==True:
-                                    lado='BUY'
-                                    ut.sound()
-                                    mensaje=par+" - "+lado+" - Hora comienzo: "+str(dt.datetime.today())
-                                    print(mensaje)
-                        else: 
-                            #CRUCE ABAJO
-                            if (((crosshigh[0]==0 and crosshigh[1]==-1 and crosshigh[2]==0 and crosshigh[3]==1) 
-                                or (crosslow[0]==0 and crosslow[1]==-1 and crosslow[2]==0 and crosslow[3]==1))
-                                and 50<df.ta.stochrsi()['STOCHRSIk_14_14_3_3'].iloc[-1]<df.ta.stochrsi()['STOCHRSId_14_14_3_3'].iloc[-1]
-                                ):
-                                                                      
                                 ut.komucloud (df)
                                 
-                                currentprice = float(client.get_symbol_ticker(symbol=par)["price"])
-                                if (df['signal'].iloc[-1]==-1 
-                                    and (df['signal'].iloc[-2]==0 or df['signal'].iloc[-2]==1)
-                                    and currentprice < df.ta.ema(50).iloc[-1] < df.ta.ema(200).iloc[-1]
-                                    ):
-                                
-                                    posicioncreada=ut.posicionfuerte(par,'SELL',client)
+                                if df['signal'].iloc[-1]==1 and (df['signal'].iloc[-2]==0 or df['signal'].iloc[-2]==-1):
+                                    posicioncreada=ut.posicionfuerte(par,'BUY',client)                                
                                     if posicioncreada==True:
-                                        lado='SELL'
+                                        lado='BUY'
                                         ut.sound()
                                         mensaje=par+" - "+lado+" - Hora comienzo: "+str(dt.datetime.today())
                                         print(mensaje)
+                        else: 
+                            #CRUCE ABAJO
+                            if float(client.get_symbol_ticker(symbol=par)["price"]) < df.ta.ema(50).iloc[-1] < df.ta.ema(200).iloc[-1]:
+                                if (((crosshigh[0]==0 and crosshigh[1]==-1 and crosshigh[2]==0 and crosshigh[3]==1) 
+                                    or (crosslow[0]==0 and crosslow[1]==-1 and crosslow[2]==0 and crosslow[3]==1))
+                                    and 50<df.ta.stochrsi()['STOCHRSIk_14_14_3_3'].iloc[-1]<df.ta.stochrsi()['STOCHRSId_14_14_3_3'].iloc[-1]
+                                    ):
+                                                                        
+                                    ut.komucloud (df)
+                                    
+                                    if df['signal'].iloc[-1]==-1 and (df['signal'].iloc[-2]==0 or df['signal'].iloc[-2]==1):                                    
+                                        posicioncreada=ut.posicionfuerte(par,'SELL',client)
+                                        if posicioncreada==True:
+                                            lado='SELL'
+                                            ut.sound()
+                                            mensaje=par+" - "+lado+" - Hora comienzo: "+str(dt.datetime.today())
+                                            print(mensaje)
 
                         if posicioncreada==True:
                             while float(exchange.fetch_balance()['info']['totalPositionInitialMargin'])!=0.0:
@@ -141,9 +133,10 @@ def main() -> None:
                                 mensaje=mensaje+"\nGanancia acumulada: "+str(ut.truncate(((float(exchange.fetch_balance()['info']['totalWalletBalance'])/saldo_inicial)-1)*100,3))+"% "+str(ut.truncate(float(exchange.fetch_balance()['info']['totalWalletBalance'])-saldo_inicial,2))+" USDT"
                                 mensaje=mensaje+"\nBalance USDT: "+str(ut.truncate(float(exchange.fetch_balance()['info']['totalWalletBalance']),3))+" USDT"
                                 mensaje=mensaje+"\nBalance BNB: "+str(ut.truncate(float((exchange.fetch_balance()['BNB']['total'])*float(client.get_symbol_ticker(symbol='BNBUSDT')["price"])),3))+" USDT"
-                                mensaje=mensaje+"\nVolumen: "+str(ut.truncate(client.futures_ticker(symbol=par)['quoteVolume']/1000000,2))+"M"
+                                mensaje=mensaje+"\nVolumen: "+str(ut.truncate(float(client.futures_ticker(symbol=par)['quoteVolume'])/1000000,2))+"M"
                                 botlaburo.send_text(mensaje)
-                            except:
+                            except Exception as a:
+                                print("Error2: "+str(a))
                                 pass
 
                             print(mensaje)
