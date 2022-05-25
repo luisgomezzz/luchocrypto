50a15e9b7a375c5db18aca898b0c98f055f3d999
lucho <luchogomezf@gmail.com>
2022-05-25 19:46:01 -0300

    completo con solo buy

--- oliver.py
+++ oliver.py
@@ -54,59 +54,28 @@ def main() -> None:
             df['ema20']=df.ta.ema(20)
             df['ema200']=df.ta.ema(200)
 
-            maximo=0.0
-            minimo=0.0
-            momento=''
             # create a new column and use np.select to assign values to it using our lists as arguments
             df['matchup'] = np.where((df.low.shift(periods=1) > df.ema5up.shift(periods=1))
                             & (df.ema5.shift(periods=1) > df.ema20.shift(periods=1))
                             & (df.ema20.shift(periods=1) > df.ema200.shift(periods=1))
                             & (df.low <= df.ema5)
-                            & (df.high.shift(periods=1)-df.low.shift(periods=1)>df.high-df.low)
+                            & ((df.high.shift(periods=1)-df.low.shift(periods=1))>(df.high-df.low))
                             , True,False)
 
             for i in df.index: 
                 if df.matchup[i]==True:
-                    if df.high[i]>=maximo:
-                        maximo=df.high.shift(periods=1)[i]
-                        minimo=df.low.shift(periods=1)[i]
-                        momento=df.time3.shift(periods=1)[i]
+                    dicciobuy[par]=[df.high.shift(periods=1)[i],df.low.shift(periods=1)[i],str(i)]
 
-            dicciobuy[par] = [maximo,minimo,str(momento)]
-
-            maximo=0.0
-            minimo=1000000.0
-            momento=''
-            # create a new column and use np.select to assign values to it using our lists as arguments
-            df['matchdown'] = np.where((df.low.shift(periods=1) < df.ema5down.shift(periods=1))
-                            & (df.ema5.shift(periods=1) < df.ema20.shift(periods=1))
-                            & (df.ema20.shift(periods=1) < df.ema200.shift(periods=1))
-                            & (df.high >= df.ema5)
-                            & (df.high.shift(periods=1)-df.low.shift(periods=1)>df.high-df.low)
-                            , True,False)
-
-            for i in df.index: 
-                if df.matchdown[i]==True:
-                    if df.low[i]<=minimo:
-                        minimo=df.low.shift(periods=1)[i]
-                        maximo=df.high.shift(periods=1)[i]
-                        momento=df.time3.shift(periods=1)[i]
-
-            dicciosell[par] = [minimo,maximo,str(momento)]
-
-            ### limpieza. si se cumplió la condicion mientras estaba tradeando entonces se borra porque se ingresaría tarde.
+            # Limpieza. Si se cumplió la condición en alguna moneda mientras 
+            # se estaba tradeando entonces se borra porque se ingresaría tarde.
             for par in list(dicciobuy):
                 precioactual= ut.currentprice(client,par)
                 if precioactual > dicciobuy[par][0]:
-                    dicciobuy.pop(par, None)
-            ###
-            ### limpieza. si se cumplió la condicion mientras estaba tradeando entonces se borra porque se ingresaría tarde.
-            for par2 in list(dicciosell):
-                precioactual= ut.currentprice(client,par2)
-                if precioactual < dicciosell[par2][0]:
-                    dicciosell.pop(par2, None)
-            ###
-                     
+                    dicciobuy.pop(par, None)               
+
+        sys.stdout.write("\rEn la mira BUY \033[K")
+        sys.stdout.flush()
+        print(dicciobuy)
 
     for s in lista_de_monedas:
         try:  
@@ -121,14 +90,9 @@ def main() -> None:
             print("\nSalida solicitada. ")
             sys.exit()
 
-    #se obtiene el historial
+    #se obtiene el historial de todas las monedas
     enlamira(lista_monedas_filtradas,porcentajevariacion,temporalidad,dicciobuy,dicciosell)
 
-    print("\nEn la mira BUY ")
-    print(dicciobuy)
-    print("\nEn la mira sell ")
-    print(dicciosell)
-    
     try:
 
         while True:
@@ -147,7 +111,6 @@ def main() -> None:
                 try:
                     try:
                         if par not in dicciobuy:
-                        #or par not in dicciosell:
 
                             sys.stdout.write("\rBuscando. Ctrl+c para salir. Par: "+par+" - Tiempo de vuelta: "+str(ut.truncate(minutes_diff,2))+" min - Monedas analizadas: "+ str(len(lista_monedas_filtradas))+"\033[K")
                             sys.stdout.flush()
@@ -162,7 +125,7 @@ def main() -> None:
                                 and df['high'].iloc[-2]-df['low'].iloc[-2]>df['high'].iloc[-1]-df['low'].iloc[-1]
                                 ):
                                 
-                                #se detectó la señal y se guarda el valor pico y low para crear posicion y stop respectivamente.
+                                #se detectó la señal y se guarda el valor pico para crear posicion cuando sea superado.
                                 dicciobuy[par] = [df['high'].iloc[-2],df['low'].iloc[-2],str(dt.datetime.today().strftime('%d/%b/%Y %H:%M:%S'))]
                                 print("\nNueva deteccion BUY ")
                                 print(dicciobuy)
@@ -176,25 +139,31 @@ def main() -> None:
                                     and df['high'].iloc[-2]-df['low'].iloc[-2]>df['high'].iloc[-1]-df['low'].iloc[-1]
                                     ):
                                 
-                                    #se detectó la señal y se guarda el valor pico y low para crear stop y posicion respectivamente.
+                                    #se detectó la señal y se guarda el low para crear posicion cuando sea superado.
                                     dicciosell[par] = [df['low'].iloc[-2],df['high'].iloc[-2],str(dt.datetime.today().strftime('%d/%b/%Y %H:%M:%S'))]
                                     print("\nNueva deteccion sell ")
-                                    print(dicciobuy)
+                                    print(dicciosell)
 
-                            if len(dicciobuy)>0:
+                            if len(dicciobuy)>0: #entro si hay señales de BUY guardadas
                                 for par2 in list(dicciobuy):
                                     
+                                    sys.stdout.write("\rEn la mira Buy. Ctrl+c para salir. Par: "+par2+"\033[K")
+                                    sys.stdout.flush()
+                                    
                                     df=ut.calculardf (par2,temporalidad,ventana)
-
-                                    #si ya hubo señal se ve si es momento de crear la posición
+                                    
                                     precioactual= ut.currentprice(client,par2)
                                     ema5=df.ta.ema(5).iloc[-1]
                                     ema20=df.ta.ema(20).iloc[-1]
                                     ema200=df.ta.ema(200).iloc[-1]
                                     ema13=df.ta.ema(13).iloc[-1]
                                     
-                                    if precioactual*(1+porcentajevariacion/100) > dicciobuy[par2][0] and ema5>ema20>ema200 and df.ta.cci(20).iloc[-1] > 100:
-                                        #si el precio actual supera el pico de la señal crear posición buy
+                                    if (
+                                        precioactual*(1+porcentajevariacion/100) > dicciobuy[par2][0] 
+                                        and ema5>ema20>ema200 
+                                        and df.ta.cci(20).iloc[-1] > 100
+                                        ):
+                                        #si ya hubo señal se ve si se dan las condiciones para que crear la posicion
                                         lado='BUY'
                                         print("\n*********************************************************************************************")
                                         mensaje="Trade - "+par2+" - "+lado
@@ -215,7 +184,6 @@ def main() -> None:
 
                                         ###############################################################################
                                         while ut.posicionesabiertas(exchange)==True:
-                                            #sleep(0.5)
                                             ut.waiting()
                                             df=ut.calculardf (par2,temporalidad,ventana)
                                             if df.ta.cci(20).iloc[-1] < 100 or ut.currentprice(client,par2) <= df.ta.ema(13).iloc[-1]:
@@ -225,16 +193,16 @@ def main() -> None:
                                         ut.closeallopenorders(client,par2)
                                         posicioncreada=False                                    
                                         
-                                        ### limpieza. si se cumplió la condicion mientras estaba tradeando entonces se borra porque se ingresaría tarde.
-                                        for par2 in list(dicciobuy):
-                                            precioactual= ut.currentprice(client,par2)
-                                            if precioactual > dicciobuy[par2][0]:
-                                                dicciobuy.pop(par2, None)
-                                        ###
-
-                                        balancetotal=ut.balancetotal(exchange,client)
+                                        # Limpieza. Si se cumplieron las condiciones en alguna moneda mientras 
+                                        # se estaba tradeando se borra porque se ingresaría tarde.
+                                        for par3 in list(dicciobuy):
+                                            precioactual= ut.currentprice(client,par3)
+                                            if precioactual > dicciobuy[par3][0]:
+                                                dicciobuy.pop(par3, None)    
 
                                         print("\nResumen: ")
+
+                                        balancetotal=ut.balancetotal(exchange,client)
                                         if balancetotal>balancegame:
                                             mensaje="\nWIN :) "+mensaje
                                         else:
@@ -255,83 +223,6 @@ def main() -> None:
 
                                         print(mensaje)
                                         print("\n*********************************************************************************************")
-                                        #sys.exit()
-    ######################################################################################################################################################3
-                            if len(dicciosell)==990:
-                                for par2 in list(dicciosell):
-
-                                    df=ut.calculardf (par2,temporalidad,ventana)
-
-                                    #si ya hubo señal se ve si es momento de crear la posición
-                                    precioactual= ut.currentprice(client,par2)
-                                    ema5=df.ta.ema(5).iloc[-1]
-                                    ema20=df.ta.ema(20).iloc[-1]
-                                    ema200=df.ta.ema(200).iloc[-1]
-                                    ema13=df.ta.ema(13).iloc[-1]
-                                    
-                                    if precioactual*(1-porcentajevariacion/100) < dicciosell[par2][0] and ema5<ema20<ema200 and df.ta.cci(20).iloc[-1] < -100:
-                                        #si el precio actual menor el low de la señal crear posición sell
-                                        lado='SELL'
-                                        print("\n*********************************************************************************************")
-                                        mensaje="Trade - "+par2+" - "+lado
-                                        mensaje=mensaje+"\nInicio: "+str(dt.datetime.today().strftime('%d/%b/%Y %H:%M:%S'))
-                                        print(mensaje)
-
-                                        stopprice = ema13
-
-                                        profitprice = precioactual-((stopprice-precioactual)/ratio)
-
-                                        posicioncreada=ut.posicioncompleta(par2,lado,client,stopprice,profitprice) 
-
-                                        balancegame=ut.balancetotal(exchange,client)
-                            
-                                    if posicioncreada==True:
-                                        
-                                        ut.sound()
-
-                                        ###############################################################################
-                                        while ut.posicionesabiertas(exchange)==True:
-                                            #sleep(0.5)
-                                            ut.waiting()
-                                            df=ut.calculardf (par2,temporalidad,ventana)
-                                            if df.ta.cci(20).iloc[-1] > -100 or ut.currentprice(client,par2) >= df.ta.ema(13).iloc[-1]:
-                                                ut.binancecierrotodo(client,par2,exchange,'BUY')
-                                        ###############################################################################
-
-                                        ut.closeallopenorders(client,par2)
-                                        posicioncreada=False
-                                        
-                                        ### limpieza. si se cumplió la condicion mientras estaba tradeando entonces se borra porque se ingresaría tarde.
-                                        for par2 in dicciosell:
-                                            precioactual= ut.currentprice(client,par2)
-                                            if precioactual < dicciosell[par2][0]:
-                                                dicciosell.pop(par2, None)
-                                        ###                          
-                                        
-                                        balancetotal=ut.balancetotal(exchange,client)
-
-                                        print("\nResumen: ")
-                                        if balancetotal>balancegame:
-                                            mensaje="\nWIN :) "+mensaje
-                                        else:
-                                            if balancetotal<balancegame:
-                                                mensaje="\nLOSE :( "+mensaje
-                                            else:
-                                                mensaje="\nNADA :| "+mensaje
-                                        try:
-                                            mensaje=mensaje+"\nCierre: "+str(dt.datetime.today().strftime('%d/%b/%Y %H:%M:%S'))
-                                            mensaje=mensaje+"\n24h Volumen: "+str(ut.truncate(float(client.futures_ticker(symbol=par2)['quoteVolume'])/1000000,1))+"M"
-                                            mensaje=mensaje+"\nGanancia sesión: "+str(ut.truncate(((balancetotal/saldo_inicial)-1)*100,3))+"% "+str(ut.truncate(balancetotal-saldo_inicial,2))+" USDT"
-                                            mensaje=mensaje+"\nBal TOTAL: "+str(ut.truncate(balancetotal,3))+" USDT - (BNB: " +str(ut.truncate(float((exchange.fetch_balance()['BNB']['total'])*float(client.get_symbol_ticker(symbol='BNBUSDT')["price"])),3))+" USDT)"
-                                            mensaje=mensaje+"\nObjetivo a: "+str(ut.truncate(balanceobjetivo-balancetotal,3))+" USDT"
-                                            botlaburo.send_text(mensaje)
-                                        except Exception as a:
-                                            print("Error2: "+str(a))
-                                            pass
-
-                                        print(mensaje)
-                                        print("\n*********************************************************************************************")
-                                        #sys.exit()                                    
 
                     except KeyboardInterrupt:
                         print("\nSalida solicitada. ")
