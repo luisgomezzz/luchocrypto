e55dee52eec2c1c598465338ee96e27d640243e7
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2022-11-03 10:07:53 -0300

    mejoras

---
+++ SANTA3/santa3.py
@@ -0,0 +1,40 @@
+#****************************************************************************************
+# version 3.0
+#
+#****************************************************************************************
+
+from this import d
+import sys, os
+sys.path.insert(1,'./')
+import util as ut
+import datetime as dt
+from datetime import datetime
+import threading
+import numpy as np
+from playsound import playsound
+
+##CONFIG
+client = ut.client
+exchange = ut.exchange
+nombrelog = "log_santa2.txt"
+operandofile = "operando.txt"
+lista_monedas_filtradas_file = "lista_monedas_filtradas.txt"
+lanzadorfile = "lanzador.py"
+## PARAMETROS FUNDAMENTALES 
+temporalidad = '1m'
+apalancamiento = 10
+margen = 'CROSSED'
+procentajeperdida = 10 #porcentaje de mi capital total maximo a perder (10)
+porcentajeentrada = 6 #porcentaje de la cuenta para crear la posición (6)
+ventana = 30 #Ventana de búsqueda en minutos.   
+porcentaje = 15 #porcentaje de variacion para el cual se dispara el trade estandar.
+cantidadcompensaciones = 2
+## VARIABLES GLOBALES 
+operando=[] #lista de monedas que se están operando
+incrementocompensacionporc = 30 #porcentaje de incremento del tamaño de la compensacion con respecto a su anterior
+balanceobjetivo = 24.00+24.88+71.53+71.62+106.01+105.3+103.14+101.55+102.03+102.49+400 #los 400 son los del prestamo del dpto que quiero recuperar
+lista_monedas_filtradas_nueva = []
+flagpuntodeataque = 0 # Ataque automatico. 0 desactivado - 1 activado 
+###################################################################################################################
+###################################################################################################################
+###################################################################################################################
--- SANTA3/util.py
+++ SANTA3/util.py
@@ -1,5 +1,7 @@
 import ccxt as ccxt
 from os import system, name
+from binance.client import Client
+from kucoin_futures.client import Trade
 
 def clear():  
     # for windows
@@ -8,8 +10,8 @@ def clear():
     # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
-
 clear()
+
 #BINANCE
 binance_key="N7yU75L3CNJg2RW0TcJBAW2cUjhPGvyuSFUgnRHvMSMMiS8WpZ8Yd8yn70evqKl0"
 binance_secret="2HfMkleskGwTb6KQn0AKUQfjBDd5dArBW3Ykd2uTeOiv9VZ6qSU2L1yWM1ZlQ5RH"
@@ -19,20 +21,22 @@ kucoin_key='63618000e26bf70001e2bd2c'
 kucoin_secret='409d3eff-9622-4488-af21-fa0feabb24ec'
 kucoin_passphares='santakucoin'
 
-exchange_name = 'kucoin'
+exchange_name = 'binance'
 
 if exchange_name == 'binance':
     api_key = binance_key
     api_secret = binance_secret
     api_passphares = binance_passphares
+    client = Client(api_key, api_secret) 
 if exchange_name == 'kucoin':
     api_key = kucoin_key
     api_secret = kucoin_secret
     api_passphares = kucoin_passphares
     exchange_name = 'kucoinfutures'
+    client = Trade(api_key, api_secret) 
 
 exchange_class = getattr(ccxt, exchange_name)
-ccxt_ex =   exchange_class({            
+exchange =   exchange_class({            
             'apiKey': api_key,
             'secret': api_secret,
             'password': api_passphares,
@@ -41,5 +45,4 @@ ccxt_ex =   exchange_class({
             },
             })
 
-balance = ccxt_ex.fetch_balance()
-print(balance)
+print(client)
