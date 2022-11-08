e87096950ce1a1067e557f6c90adc3647c088ef1
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2022-11-08 11:22:56 -0300

    ignore

--- SANTA3/lanzador.py
+++ SANTA3/lanzador.py
@@ -1,10 +1,10 @@
 
-# https://www.binance.com/en/futures/FTTUSDT
-# https://www.tradingview.com/chart/Wo0HiKnm/?symbol=BINANCE%3AFTTUSDT
+# https://www.binance.com/en/futures/CHZUSDTM
+# https://www.tradingview.com/chart/Wo0HiKnm/?symbol=BINANCE%3ACHZUSDTM
 import sys
 sys.path.insert(1,'./')
 import santa3 as san
-par='FTTUSDT'
+par='CHZUSDTM'
 lado='BUY'
 #san.trading(par,lado,10)
 san.updating(par,lado)
