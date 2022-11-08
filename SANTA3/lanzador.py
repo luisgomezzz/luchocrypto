8c2f27f84f34eee8e7cab2f8f99169ee40984d13
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2022-11-08 12:01:58 -0300

    cambios

--- SANTA3/lanzador.py
+++ SANTA3/lanzador.py
@@ -1,10 +1,10 @@
 
-# https://www.binance.com/en/futures/OPUSDT
-# https://www.tradingview.com/chart/Wo0HiKnm/?symbol=BINANCE%3AOPUSDT
+# https://www.binance.com/en/futures/DOGEUSDT
+# https://www.tradingview.com/chart/Wo0HiKnm/?symbol=BINANCE%3ADOGEUSDT
 import sys
 sys.path.insert(1,'./')
 import santa3 as san
-par='OPUSDT'
-lado='SELL'
+par='DOGEUSDT'
+lado='BUY'
 #san.trading(par,lado,10)
 san.updating(par,lado)
