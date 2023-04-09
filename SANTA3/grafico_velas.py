4d536d69a29ddeb72c55bc52c1ef2f8aabb26585
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2023-04-08 21:35:54 -0300

    mejoras en grafica

--- SANTA3/grafico_velas.py
+++ SANTA3/grafico_velas.py
@@ -28,19 +28,29 @@ df['regimeFilter'] = df.close > (df.emaweek + (df.atrValue * 0.25))
 # Calculate RSI
 df['rsiValue'] = pta.rsi(df['close'], length=7)
 # Get bullish momentum filter
-df['bullish'] = df.regimeFilter.any() and (df.rsiValue.any() > rsiMom or not useRsi)
+df['bullish'] = df['regimeFilter'] & (df.rsiValue.any() > rsiMom or not useRsi)
 # Define bearish caution filter
 df['caution'] = df['bullish'] & ((df['high'].rolling(window=7).max() - df['low']) > (df['atrValue'] * 1.5))
 # Set momentum color
 df['bgCol'] = 'red'
-if df.bullish.iloc[-2]:
-    df['bgCol'] = 'green'
-if df.caution.iloc[-2]:
-    df['bgCol'] = 'orange'
-print(df)
+df['bgCol'] = np.where(df['bullish'].shift(-1)==True,'green',df['bgCol'])
+df['bgCol'] = np.where(df['caution'].shift(-1)==True,'orange',df['bgCol'])
+# Set trailing stop loss
+df['trailStop'] = np.nan
+#Handle strategy entry
+df['position_size']=0
+df['buy_sell'] = np.where(df['bullish'] & ~df.caution & df.position_size == 0,'buy',np.nan)
+df['trailStop'] = np.where(df['buy_sell'] =='buy',np.nan,df['trailStop'])
+# Handle trailing stop
+df['temp_trailStop'] = df['low'].rolling(window=7).max() - np.where(df.caution.shift(-1), df.atrValue * 0.2 , df.atrValue)
+df['trailStop'] = np.where(df.position_size > 0,np.where((df.temp_trailStop > df.trailStop) | (df.trailStop == np.nan),df['temp_trailStop'],df['trailStop']),df['trailStop'])
+# Handle strategy exit
+df['buy_sell'] = np.where((df.close < df.trailStop) | (df.close < df.emaweek), 'sell', np.nan)
+
+print(df.tail(90))
 
 fig = make_subplots(specs=[[{"secondary_y": True}]])
-
+# velas
 fig.add_trace(
             go.Candlestick(
             x=df['timestamp'],
@@ -51,8 +61,7 @@ fig.add_trace(
         ),
                secondary_y=True
 )
-
-# include a go.Bar trace for volumes
+# volume
 fig.add_trace(go.Bar(
     x=df.index,
     y=df['volume'],
@@ -62,7 +71,7 @@ fig.add_trace(go.Bar(
     }
 ),
 secondary_y=False)
-
+# ema weekly
 fig.add_trace(
             go.Scatter(
             x=df['timestamp'],
@@ -70,13 +79,24 @@ fig.add_trace(
             mode='markers+lines',
             marker={'color': df['color_emaweek']}, 
             line={'color': 'gray'},
-            name='EMA(20)'
+            name='EMA(20) weekly'
         ),
                secondary_y=True
 )
+# momentum
+fig.add_trace(
+            go.Scatter(
+            x=df['timestamp'],
+            y=df['emaweek'],
+            mode='markers+lines',
+            marker={'color': df['bgCol']}, 
+            line={'color': 'gray'},
+            name='Momentum Strength'
+        ),
+               secondary_y=False
+)
 
-fig.layout.yaxis2.showgrid=False
-fig.update_layout(title="ETH/USDC pool after Uniswap v3 deployment", height=800)
+fig.update_layout(title=f"{symbol}", height=800)
 fig.update_yaxes(title="Price $", secondary_y=True, showgrid=True)
 fig.update_yaxes(title="Volume $", secondary_y=False, showgrid=False)
 fig.show()
