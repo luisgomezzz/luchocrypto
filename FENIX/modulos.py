0125ec9d7216b4823b4b7ab92059f1657ad285ef
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2023-07-28 17:36:40 -0300

    se agrega estrategia triangulos

--- FENIX/modulos.py
+++ FENIX/modulos.py
@@ -614,3 +614,121 @@ def estrategia_santa(symbol,tp_flag = True):
         )
     )    
     return data    
+
+def estrategia_triangulos(symbol,tp_flag = False):
+    from scipy.stats import linregress
+    #por defecto está habilitado el tp pero puede sacarse a mano durante el trade si el precio va a favor dejando al trailing stop como profit
+    np.seterr(divide='ignore', invalid='ignore')
+    timeframe = '15m'    
+
+    def pivotid(df1, l, n1, n2): #n1 n2 before and after candle l
+        if l-n1 < 0 or l+n2 >= len(df1):
+            return 0    
+        pividlow=1
+        pividhigh=1
+        for i in range(l-n1, l+n2+1):
+            if(df1.Low[l]>df1.Low[i]):
+                pividlow=0
+            if(df1.High[l]<df1.High[i]):
+                pividhigh=0
+        if pividlow and pividhigh:
+            return 3
+        elif pividlow:
+            return 1
+        elif pividhigh:
+            return 2
+        else:
+            return 0    
+
+    def pointpos(x):
+        if x['pivot']==1:
+            return x['Low']-1e-3
+        elif x['pivot']==2:
+            return x['High']+1e-3
+        else:
+            return np.nan    
+        
+    df = obtiene_historial(symbol,'15m')
+    #Check if NA values are in data
+    df=df[df['Volume']!=0]
+    df.reset_index(drop=True, inplace=True)
+    df.isna().sum()
+    df["pivot"] = df.apply(lambda x: pivotid(df, x.name,3,3), axis=1)
+    df["pointpos"] = df.apply(lambda row: pointpos(row), axis=1)
+    df["signal"]=0
+    df["upper_line"]=0
+    df["lower_line"]=0
+    ### detecta la vela donde hay un triangulo a partir de la posicion que se elija en el rango
+    backcandles= 20
+    for candleid in range(0, len(df)):
+        maxim = np.array([])
+        minim = np.array([])
+        xxmin = np.array([])
+        xxmax = np.array([])
+        for i in range(candleid-backcandles, candleid+1):
+            if df.iloc[i].pivot == 1:
+                minim = np.append(minim, df.iloc[i].Low)
+                xxmin = np.append(xxmin, i) #could be i instead df.iloc[i].name
+            if df.iloc[i].pivot == 2:
+                maxim = np.append(maxim, df.iloc[i].High)
+                xxmax = np.append(xxmax, i) # df.iloc[i].name
+        
+        if (xxmax.size <3 and xxmin.size <3) or xxmax.size==0 or xxmin.size==0:
+            continue
+        
+        slmin, intercmin, rmin, pmin, semin = linregress(xxmin, minim)
+        slmax, intercmax, rmax, pmax, semax = linregress(xxmax, maxim)
+            
+        if abs(rmax)>=0.7 and abs(rmin)>=0.7 and abs(slmin)<=0.00001 and slmax<-0.0001:
+            print(f"{symbol} - {candleid} - triangulo desc")
+            df.loc[[candleid],'signal'] = 2
+            #break
+        if abs(rmax)>=0.7 and abs(rmin)>=0.7 and slmin>=0.0001 and abs(slmax)<=0.00001:
+            print(f"{symbol} - {candleid} - triangulo asc")
+            df.loc[[candleid],'signal'] = 3
+            #break
+        if abs(rmax)>=0.9 and abs(rmin)>=0.9 and slmin>=0.0001 and slmax<=-0.0001:
+            print(f"{symbol} - {candleid} - triangulo comun")
+            df.loc[[candleid],'signal'] = 4
+            #break  
+        if df.iloc[candleid].signal in (2,3,4):
+            # Ecuación de la línea superior
+            xssup = xxmax
+            yssup = slmax*xxmax + intercmax
+            pendiente = (yssup[1]-yssup[0])/(xssup[1]-xssup[0])
+            intersecciony = yssup[0]-pendiente*xssup[0]
+            df.loc[[candleid],'upper_line']=pendiente*candleid+intersecciony        
+            print(f"linea superior {df.iloc[candleid].upper_line}")        
+            # Ecuación de la línea inferior
+            xsinf = xxmin
+            ysinf = slmin*xxmin + intercmin
+            pendiente = (ysinf[1]-ysinf[0])/(xsinf[1]-xsinf[0])
+            intersecciony = ysinf[0]-pendiente*xsinf[0]
+            df.loc[[candleid],'lower_line']=pendiente*candleid+intersecciony
+            print(f"linea inferior {df.iloc[candleid].lower_line}")
+            if df.iloc[candleid].Close < df.iloc[candleid].lower_line and df.iloc[candleid].Close < df.iloc[candleid].upper_line:
+                df.loc[[candleid],'signal'] = -1
+            elif df.iloc[candleid].Close > df.iloc[candleid].lower_line and df.iloc[candleid].Close > df.iloc[candleid].upper_line:
+                df.loc[[candleid],'signal'] = 1
+
+    df['take_profit'] =   np.where(
+                            tp_flag,np.where(
+                            df.signal == 1,
+                            df.Close + 3*df.atr,
+                            np.where(
+                                    df.signal == -1,
+                                    df.Close - 3*df.atr,  
+                                    0
+                                    )
+                            ),np.NaN
+                                    )
+    df['stop_loss'] = np.where(
+        df.signal == 1,
+        df.Close - 5*df.atr,    # se exagera colocando 5 ya que el stop lo realiza el trailing
+        np.where(
+            df.signal == -1,
+            df.Close + 5*df.atr,
+            0
+        )
+    )    
+    return df        
