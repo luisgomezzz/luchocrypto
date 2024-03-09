9f30ebc303394c1bfdda18057a2f2372d890815f
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2024-03-08 21:39:56 -0300

    variacion btc

--- FENIX/smarto.py
+++ FENIX/smarto.py
@@ -5,141 +5,193 @@ import sys
 import constantes as cons
 import warnings
 from time import sleep
+import threading
 
 def warn(*args, **kwargs):
     pass
 warnings.warn = warn
 np.seterr(divide='ignore')
-lista_filtrada = []
 
-while True:
-    print(f"Filtrando monedas...")
-    lista = md.filtradodemonedas ()
-    #lista = ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'LINKUSDT', 'BNBUSDT', 'SOLUSDT', 'ARBUSDT', 'SUIUSDT', 'ORDIUSDT', '1000SATSUSDT', 'MANTAUSDT', 'ALTUSDT', 'JUPUSDT']
-    #lista = ['XRPUSDT']
+def filtradodemonedas ():
+    import warnings
+    sys.path.insert(0, 'C:/LUCHO/personal/repopersonal/luchocrypto/FENIX')
+    import modulos as md  
+    warnings.filterwarnings("ignore")
+    try:
+        symbol='BTCUSDT'
+        timeframe = '1m'
+        df = md.obtiene_historial(symbol, timeframe)
+        df = df.tail(30)
+        preciomenor=df.Close.min()
+        preciomayor=df.Close.max()
+        timestampmaximo=max(df[df['Close']==max( df['Close'])]['Open Time'])
+        timestampminimo=max(df[df['Close']==min( df['Close'])]['Open Time'])
+        if timestampmaximo>=timestampminimo:    
+            variacion = md.truncate(((preciomayor/preciomenor)-1)*100,2)
+        else:    
+            variacion = md.truncate(-((preciomenor/preciomayor)-1)*-100,2)
+        if abs(variacion)>=1.5:
+            print("\nALTA VARIACION DE BTC!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+            anuncio=True
+        else:
+            anuncio=False
+        sys.stdout.write("\rVariación BTC:"+str(variacion)+"%")
+        sys.stdout.flush()              
+        global anuncioaltavariacionbtc
+        anuncioaltavariacionbtc = anuncio            
+    except Exception as ex:
+        pass        
+    except KeyboardInterrupt as ky:
+        print("\nSalida solicitada. ")
+        sys.exit() 
 
-    timeframe = '1h'
+def loopfiltradodemonedas ():
+    while True:
+        filtradodemonedas ()
 
-    if len(lista) > 1:
-        imprimo = False
-        debug = False
-    else:
-        imprimo = True
-        debug = True
-    print(lista)
-    porcentajes_sumados = 0
-    win_rate_buenos = 0
-    win_rate_malos = 0
-    trades = 0
-    balance = int(md.balancetotal())
-    tp_multiplicador = 18
-    lejania = 6
-    # posiciones abiertas    
-    posiciones_abiertas = md.get_posiciones_abiertas()
+def main() -> None:    
+    lista_filtrada = []
+    anuncioaltavariacionbtc = False  # va a ser global
 
-    # obtiene una lista de las ordenes actuales
-    open_orders = cons.client.futures_get_open_orders() 
-    lista_ordenes_acutales =[]
-    for dato in open_orders:    
-        if dato['symbol'] not in lista_ordenes_acutales:
-            lista_ordenes_acutales.append(dato['symbol'])
+        #lanza filtrado de monedas paralelo
+    hilofiltramoneda = threading.Thread(target=loopfiltradodemonedas)
+    hilofiltramoneda.daemon = True
+    hilofiltramoneda.start()      
 
-    # cierra las ordenes que no estén en trades abiertos
-    for i in lista_ordenes_acutales:
-        if i not in posiciones_abiertas:
-            md.closeallopenorders (i)
+    while True:
+        print(f"Filtrando monedas...")
+        lista = md.filtradodemonedas ()
+        timeframe = '1h'
+        if len(lista) > 1:
+            imprimo = False
+            debug = False
+        else:
+            imprimo = True
+            debug = True
+        print(lista)
+        porcentajes_sumados = 0
+        win_rate_buenos = 0
+        win_rate_malos = 0
+        trades = 0
+        balance = int(md.balancetotal())
+        tp_multiplicador = 18
+        lejania = 6
+        # posiciones abiertas    
+        posiciones_abiertas = md.get_posiciones_abiertas()
 
-    # si hay una posición abierta y no está en la lista de monedas filtradas porque ya no comple las condiciones 
-    # entonces la agrego ya que debo crear el tp.
-    for i in posiciones_abiertas.keys():
-        if i not in lista:
-            lista.append(i)
+        # obtiene una lista de las ordenes actuales
+        open_orders = cons.client.futures_get_open_orders() 
+        lista_ordenes_acutales =[]
+        for dato in open_orders:    
+            if dato['symbol'] not in lista_ordenes_acutales:
+                lista_ordenes_acutales.append(dato['symbol'])
 
-    for symbol in lista:    
-        try:
-            data = md.estrategia_smart(symbol, debug = False, refinado = False, fuente = 0, timeframe = timeframe, largo = 1)
-            if symbol not in posiciones_abiertas:
-                crear_orden = False                
-                resultado = md.backtesting_smart(data, plot_flag=imprimo, symbol=symbol)
-                if resultado['Win Rate [%]'] >= 50:
-                    lista_filtrada.append(symbol)
-                print(f"{symbol} - Return [%]: {md.truncate(resultado['Return [%]'],2)}% - # Trades: {resultado['# Trades']} - Profit Factor: {resultado['Profit Factor']} - Win Rate [%]: {resultado['Win Rate [%]']}")
-                porcentajes_sumados=porcentajes_sumados+resultado['Return [%]']
-                trades = trades + resultado['# Trades']
-                if not np.isnan(resultado['Win Rate [%]']):
+        # cierra las ordenes que no estén en trades abiertos
+        for i in lista_ordenes_acutales:
+            if i not in posiciones_abiertas:
+                md.closeallopenorders (i)
+
+        # si hay una posición abierta y no está en la lista de monedas filtradas porque ya no comple las condiciones 
+        # entonces la agrego ya que debo crear el tp.
+        for i in posiciones_abiertas.keys():
+            if i not in lista:
+                lista.append(i)
+
+        for symbol in lista:    
+            try:
+                data = md.estrategia_smart(symbol, debug = False, refinado = False, fuente = 0, timeframe = timeframe, largo = 1)
+                if symbol not in posiciones_abiertas:
+                    crear_orden = False                
+                    resultado = md.backtesting_smart(data, plot_flag=imprimo, symbol=symbol)
                     if resultado['Win Rate [%]'] >= 50:
-                        win_rate_buenos = win_rate_buenos+1
-                    else:
-                        win_rate_malos = win_rate_malos+1
-                ## para smart        
-                if (### LONG
-                    data.trend.iloc[-2]=='Alcista' and
-                    data.Close.iloc[-1] < (data.decisional_alcista_high.iloc[-2] + (data.atr.iloc[-1]*lejania))):
-                    print(f"posible entrada long symbol: {symbol}")   
-                    crear_orden=True
-                    size = balance * data.porcentajeentrada_alcista.iloc[-2]/100
-                    precio = md.RoundToTickUp(symbol,data.decisional_alcista_high.iloc[-2] + data.offset.iloc[-2])
-                    sl= md.RoundToTickUp(symbol,data.decisional_alcista_low.iloc[-2] - data.offset.iloc[-2])
-                    tp = md.RoundToTickUp(symbol,data.decisional_alcista_high.iloc[-2] + data.atr.iloc[-2]*tp_multiplicador)
-                    tamanio = md.truncate((size/precio),md.get_quantityprecision(symbol))   
-                    side = 'BUY'
-                elif (### SHORT
-                        data.trend.iloc[-2]=='Bajista' and
-                        data.Close.iloc[-1] > (data.decisional_bajista_low.iloc[-2] - (data.atr.iloc[-1]*lejania))):
-                        print(f"posible entrada short symbol: {symbol}")
+                        lista_filtrada.append(symbol)
+                    print(f"{symbol} - Return [%]: {md.truncate(resultado['Return [%]'],2)}% - # Trades: {resultado['# Trades']} - Profit Factor: {resultado['Profit Factor']} - Win Rate [%]: {resultado['Win Rate [%]']}")
+                    porcentajes_sumados=porcentajes_sumados+resultado['Return [%]']
+                    trades = trades + resultado['# Trades']
+                    if not np.isnan(resultado['Win Rate [%]']):
+                        if resultado['Win Rate [%]'] >= 50:
+                            win_rate_buenos = win_rate_buenos+1
+                        else:
+                            win_rate_malos = win_rate_malos+1
+                    ## para smart        
+                    if (### LONG
+                        data.trend.iloc[-2]=='Alcista' and
+                        data.Close.iloc[-1] < (data.decisional_alcista_high.iloc[-2] + (data.atr.iloc[-1]*lejania))):
+                        print(f"posible entrada long symbol: {symbol}")   
                         crear_orden=True
-                        size = balance * data.porcentajeentrada_bajista.iloc[-2]/100
-                        precio = md.RoundToTickUp(symbol,data.decisional_bajista_low.iloc[-2] - data.offset.iloc[-2])
-                        sl= md.RoundToTickUp(symbol,data.decisional_bajista_high.iloc[-2] + data.offset.iloc[-2])
-                        tp = md.RoundToTickUp(symbol,data.decisional_bajista_low.iloc[-2] - data.atr.iloc[-2]*tp_multiplicador)
-                        tamanio = md.truncate((size/precio),md.get_quantityprecision(symbol))
-                        side = 'SELL'
-                ### creacion de ordenes
-                if crear_orden==True:                    
-                    cons.client.futures_create_order(symbol=symbol,
-                        side=side,
-                        type='LIMIT',
-                        quantity=tamanio,
-                        timeInForce='GTC',
-                        price=precio
-                    )
-                    # Crear la orden Stop-Loss
-                    orden_stop_loss = cons.client.futures_create_order(
-                        symbol=symbol,
-                        side=np.where(side == 'BUY','SELL','BUY'),
-                        type='STOP_MARKET',
-                        quantity=tamanio,
-                        stopPrice=sl,
-                        closePosition=True
-                    )
-            else:
-                # crea TP para las órdenes abiertas si no lo poseen
-                tps_creados = 0
-                open_limits = cons.client.futures_get_open_orders(symbol=symbol)     
-                for orden in open_limits:
-                    if orden['type'] == 'TAKE_PROFIT_MARKET':
-                        tps_creados = tps_creados + 1
-                if tps_creados == 0:
-                    lado = posiciones_abiertas[symbol]
-                    if lado == 'BUY':
-                        tp = data['Close'].rolling(40).max().iloc[-1]
-                    else:
-                        tp = data['Close'].rolling(40).min().iloc[-1]
-                    print(f"creo tp para {symbol}")
-                    orden_take_profit = cons.client.futures_create_order(
-                                                                        symbol=symbol,
-                                                                        side=np.where(lado == 'BUY','SELL','BUY'),
-                                                                        type='TAKE_PROFIT_MARKET',
-                                                                        stopPrice=tp,
-                                                                        closePosition=True
-                                                                        )
-        except Exception as falla:
-            _, _, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            print("Error: "+str(falla)+" - line: "+str(exc_tb.tb_lineno)+" - file: "+str(fname))
-            pass
-    for symbol in lista_filtrada:
-        md.printandlog(cons.nombrelog,symbol,pal=1)
-    print(f"Timeframe {timeframe} - porcentajes_sumados {md.truncate(porcentajes_sumados,2)} - trades {trades} - win_rate_buenos {win_rate_buenos} - win_rate_malos {win_rate_malos} - Ganancia por trade: {md.truncate((porcentajes_sumados/trades if trades !=0 else porcentajes_sumados),2)}%")
-    print("duermo 30 minutos...")
-    sleep(1800)
+                        size = balance * data.porcentajeentrada_alcista.iloc[-2]/100
+                        precio = md.RoundToTickUp(symbol,data.decisional_alcista_high.iloc[-2] + data.offset.iloc[-2])
+                        sl= md.RoundToTickUp(symbol,data.decisional_alcista_low.iloc[-2] - data.offset.iloc[-2])
+                        tp = md.RoundToTickUp(symbol,data.decisional_alcista_high.iloc[-2] + data.atr.iloc[-2]*tp_multiplicador)
+                        tamanio = md.truncate((size/precio),md.get_quantityprecision(symbol))   
+                        side = 'BUY'
+                    elif (### SHORT
+                            data.trend.iloc[-2]=='Bajista' and
+                            data.Close.iloc[-1] > (data.decisional_bajista_low.iloc[-2] - (data.atr.iloc[-1]*lejania))):
+                            print(f"posible entrada short symbol: {symbol}")
+                            crear_orden=True
+                            size = balance * data.porcentajeentrada_bajista.iloc[-2]/100
+                            precio = md.RoundToTickUp(symbol,data.decisional_bajista_low.iloc[-2] - data.offset.iloc[-2])
+                            sl= md.RoundToTickUp(symbol,data.decisional_bajista_high.iloc[-2] + data.offset.iloc[-2])
+                            tp = md.RoundToTickUp(symbol,data.decisional_bajista_low.iloc[-2] - data.atr.iloc[-2]*tp_multiplicador)
+                            tamanio = md.truncate((size/precio),md.get_quantityprecision(symbol))
+                            side = 'SELL'
+                    ### creacion de ordenes
+                    if crear_orden==True:                    
+                        cons.client.futures_create_order(symbol=symbol,
+                            side=side,
+                            type='LIMIT',
+                            quantity=tamanio,
+                            timeInForce='GTC',
+                            price=precio
+                        )
+                        # Crear la orden Stop-Loss
+                        orden_stop_loss = cons.client.futures_create_order(
+                            symbol=symbol,
+                            side=np.where(side == 'BUY','SELL','BUY'),
+                            type='STOP_MARKET',
+                            quantity=tamanio,
+                            stopPrice=sl,
+                            closePosition=True
+                        )
+                else:
+                    # crea TP para las órdenes abiertas si no lo poseen
+                    tps_creados = 0
+                    open_limits = cons.client.futures_get_open_orders(symbol=symbol)     
+                    for orden in open_limits:
+                        if orden['type'] == 'TAKE_PROFIT_MARKET':
+                            tps_creados = tps_creados + 1
+                    if tps_creados == 0:
+                        lado = posiciones_abiertas[symbol]
+                        if lado == 'BUY':
+                            tp = data['Close'].rolling(40).max().iloc[-1]
+                        else:
+                            tp = data['Close'].rolling(40).min().iloc[-1]
+                        print(f"creo tp para {symbol}")
+                        orden_take_profit = cons.client.futures_create_order(
+                                                                            symbol=symbol,
+                                                                            side=np.where(lado == 'BUY','SELL','BUY'),
+                                                                            type='TAKE_PROFIT_MARKET',
+                                                                            stopPrice=tp,
+                                                                            closePosition=True
+                                                                            )
+            except Exception as falla:
+                _, _, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print("Error: "+str(falla)+" - line: "+str(exc_tb.tb_lineno)+" - file: "+str(fname))
+                pass
+        for symbol in lista_filtrada:
+            md.printandlog(cons.nombrelog,symbol,pal=1)
+        print(f"Timeframe {timeframe} - porcentajes_sumados {md.truncate(porcentajes_sumados,2)} - trades {trades} - win_rate_buenos {win_rate_buenos} - win_rate_malos {win_rate_malos} - Ganancia por trade: {md.truncate((porcentajes_sumados/trades if trades !=0 else porcentajes_sumados),2)}%")
+        print("duermo 30 minutos...")
+        cuentasegundos=1800
+        while cuentasegundos > 0:
+            sleep(1)
+            cuentasegundos=cuentasegundos-1
+            if anuncioaltavariacionbtc == True:
+                cuentasegundos=1800
+            sys.stdout.write("\r"+str(cuentasegundos)+" segundos - anuncioaltavariacionbtc: "+str(anuncioaltavariacionbtc))
+            sys.stdout.flush()  
+
+if __name__ == '__main__':
+    main()
