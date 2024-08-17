12f6a79b6a76f566c1529e8217f0c370a6d6b298
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2024-08-17 16:26:25 -0300

    nuevo

--- CRYPTON/constantes.py
+++ CRYPTON/constantes.py
@@ -1,7 +1,6 @@
 from os import system, name
 import os
-import ccxt as ccxt
-from binance.client import Client as binanceClient
+from binance.client import Client 
 
 pathroot=os.path.dirname(os.path.abspath(__file__))+'/'
 
@@ -23,22 +22,6 @@ f.close()
 api_key="NYRJeyPdSulRAL1Chx79V36zXcZ6TmhKGZGkoU2vQSq9elCyJqjq8c8ixywv5Yn0"
 api_secret="5Omvt1ChDkW0ddH51s8X6tXb4rei6TfL3kCllnDFl2Tm3Q3noECOyL6uVzBpxMqJ"
 api_passphares=''
-exchange_name = 'binance'
-client = binanceClient(api_key, api_secret,api_passphares)
-exchange_class = getattr(ccxt, exchange_name)
-exchange =   exchange_class({            
-            'apiKey': api_key,
-            'secret': api_secret,
-            'password': api_passphares,
-            'options': {  
-            'defaultType': 'future',  
-            },
-            })
+cliente = Client(api_key, api_secret,api_passphares)
 
 margen = 'CROSSED'
-
-minvolumen24h = float(100000000)
-mincapitalizacion = float(100000000)
-
-#monedas que no quiero operar
-mazmorra=[]
