5ae0d259647254e82d0e4c991ebe2b36720e9ab2
Gofre <75710496+luisgomezzz@users.noreply.github.com>
2024-10-17 05:21:41 -0300

    mejora

--- CRYPTON/estrategias.py
+++ CRYPTON/estrategias.py
@@ -44,29 +44,32 @@ def estrategia_lucho (symbol,timeframe):
     data['porcentajeentrada'] = np.where(((porcentaje_perdida/variacion_hasta_stop_loss))>=1,0.99,((porcentaje_perdida/variacion_hasta_stop_loss)))
     return data
 
-def estrategia_adx (symbol,timeframe,length_sma = 50, order = 550, length_adx = 100):
-    data = util.obtiene_historial2(symbol=symbol, timeframe=timeframe, start_date='2023-09-01', end_date=None)
-    multiplicador_tp = 7
-    data['Indicator1'] = None
-    data['Indicator2'] = ta.sma(data.Close, length = length_sma)
-    data['adx'] = ta.adx(high=data.High, low=data.Low, close=data.Close, length = length_adx).iloc[:, 0]
-    # Encontrar los índices de los mínimos locales en la columna 'adx'
-    min_indices = argrelextrema(data['adx'].values, np.less,order=order)[0]
-    data['pico_inferior'] = np.nan
-    # Marcar los picos inferiores en la columna 'pico_inferior' usando iloc
-    data.iloc[min_indices, data.columns.get_loc('pico_inferior')] = data.iloc[min_indices, data.columns.get_loc('adx')]
-    umbral=np.ceil(data['pico_inferior'].iloc[min_indices].mean())
-    data['adx_condicion'] = np.where((data.adx >= umbral) & (data.adx.shift(1) < umbral), True, False)
-    ##################################    obligatorios ##############  --->>>  trade, stop_loss, take_profit, porcentajeentrada, cerrar
-    ###########################################################################################################################
-    porcentaje_perdida = 1
-    data['trade'] = np.where(data.adx_condicion, np.where((data.Close < data.Indicator2), -2, np.where((data.Close > data.Indicator2), -1, -1.5)), -1.5)
-    data['stop_loss'] = np.where((data.trade!=-1.5) & data.Indicator2.notna(), data.Indicator2
-                                 ,np.where(data.trade==-1,data.Close-data.atr*4
-                                           ,np.where(data.trade==-2,data.Close+data.atr*4,0)))
-    variacion_hasta_stop_loss = np.where(data.trade==-1,(((data.stop_loss/data.Close)-1)*-100),np.where(data.trade==-2,(((data.stop_loss/data.Close)-1)*100),0))
-    data['take_profit'] = np.where(data.trade==-1,data.Close*(1+(variacion_hasta_stop_loss*multiplicador_tp/100)),np.where(data.trade==-2,data.Close*(1-(variacion_hasta_stop_loss*multiplicador_tp/100)),0))
-    data['porcentajeentrada'] = np.where(((porcentaje_perdida/variacion_hasta_stop_loss))>=1,0.99,((porcentaje_perdida/variacion_hasta_stop_loss)))
-    data['cerrar'] = np.where(util.crossover_dataframe(data.Close, data.Indicator2) | util.crossover_dataframe(data.Indicator2, data.Close), True, False)
-    #print(f'umbral: {umbral}')
+def estrategia_adx (symbol,timeframe,length_sma = 50, order = 550, length_adx = 90, start_date='2023-09-01'):
+    try:
+        data = util.obtiene_historial2(symbol=symbol, timeframe=timeframe, start_date = start_date, end_date=None)
+        multiplicador_tp = 7
+        data['Indicator1'] = None
+        data['Indicator2'] = ta.sma(data.Close, length = length_sma)
+        data['adx'] = ta.adx(high=data.High, low=data.Low, close=data.Close, length = length_adx).iloc[:, 0]
+        # Encontrar los índices de los mínimos locales en la columna 'adx'
+        min_indices = argrelextrema(data['adx'].values, np.less,order=order)[0]
+        data['pico_inferior'] = np.nan
+        # Marcar los picos inferiores en la columna 'pico_inferior' usando iloc
+        data.iloc[min_indices, data.columns.get_loc('pico_inferior')] = data.iloc[min_indices, data.columns.get_loc('adx')]
+        umbral=np.ceil(data['pico_inferior'].iloc[min_indices].mean())
+        data['adx_condicion'] = np.where((data.adx >= umbral) & (data.adx.shift(1) < umbral), True, False)
+        ##################################    obligatorios ##############  --->>>  trade, stop_loss, take_profit, porcentajeentrada, cerrar
+        ###########################################################################################################################
+        porcentaje_perdida = 1
+        data['trade'] = np.where(data.adx_condicion, np.where((data.Close < data.Indicator2), -2, np.where((data.Close > data.Indicator2), -1, -1.5)), -1.5)
+        data['stop_loss'] = np.where((data.trade!=-1.5) & data.Indicator2.notna(), data.Indicator2
+                                    ,np.where(data.trade==-1,data.Close-data.atr*4
+                                            ,np.where(data.trade==-2,data.Close+data.atr*4,0)))
+        variacion_hasta_stop_loss = np.where(data.trade==-1,(((data.stop_loss/data.Close)-1)*-100),np.where(data.trade==-2,(((data.stop_loss/data.Close)-1)*100),0))
+        data['take_profit'] = np.where(data.trade==-1,data.Close*(1+(variacion_hasta_stop_loss*multiplicador_tp/100)),np.where(data.trade==-2,data.Close*(1-(variacion_hasta_stop_loss*multiplicador_tp/100)),0))
+        data['porcentajeentrada'] = np.where(((porcentaje_perdida/variacion_hasta_stop_loss))>=1,0.99,((porcentaje_perdida/variacion_hasta_stop_loss)))
+        data['cerrar'] = np.where(util.crossover_dataframe(data.Close, data.Indicator2) | util.crossover_dataframe(data.Indicator2, data.Close), True, False)
+        print(f'umbral: {umbral}')
+    except Exception as e:
+        print(f"Error en {symbol}: {e}")
     return data
